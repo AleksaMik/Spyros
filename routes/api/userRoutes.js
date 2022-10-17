0af8921426414fd2c0797b0ab2001a075c689e2f
AleksaMik <aleksam.2509@gmail.com>
2022-10-17 12:26:50 -0500

    userRoutes.js upd

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,17 @@
+const router = require ('express').Router;
+const {
+    getUsersById,
+    getAllUsers,
+    createUsers,
+    updateUsers,
+    deleteUsers,
+    addFriend,
+    deleteFriend
+
+} = require ('../../controllers/users-controllers');
+
+router.route('/').get(getAllUsers).post(createUsers);
+router.route ('/:id').get(getUsersById).put(updateUsers).delete(deleteUsers);
+router.route('/:id/friends/friendId').post(addFriend).delete(deleteFriend);
+
+module.esxport = router;
