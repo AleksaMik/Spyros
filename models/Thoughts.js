b04ab9186d70543490ff1c2932e08fded196d0db
AleksaMik <aleksam.2509@gmail.com>
2022-10-17 16:13:26 -0500

    model/thoughts.js upd

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -30,8 +30,6 @@ const ReactionsSchema = new Schema(
     } 
     }
 );
-
-// ThoughtsSchema
 const ThoughtsSchema = new Schema(
     {
     thoughtText: {
@@ -43,7 +41,6 @@ const ThoughtsSchema = new Schema(
     createdAt: {
         type: Date,
         default: Date.now,
-        // Moment
         get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
     },
     username: {
