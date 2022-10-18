f5153f3e72c589e38a96abd5886ef7eaae398ce5
AleksaMik <aleksam.2509@gmail.com>
2022-10-17 21:50:31 -0500

    server.js update

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,12 +1,13 @@
-const {Users} = require('../models');
+const {User} = require('../models');
 const usersController = {
+    
     createUsers({body}, res) {
         Users.create(body)
         .then(dbUsersData => res.json(dbUsersData))
         .catch(err => res.status(400).json(err));
     },
     getAllUsers(req, res) {
-        Users.find({})
+        User.find({})
         .populate({path: 'thoughts', select: '-__v'})
         .populate({path: 'friends', select: '-__v'})
         .select('-__v')
@@ -16,13 +17,11 @@ const usersController = {
             res.status(500).json(err);
         });
     },
-
     getUsersById({params}, res) {
         Users.findOne({_id: params.id })
         .populate({path: 'thoughts', select: '-__v'})
         .populate({path: 'friends', select: '-__v'})
         .select('-__v')
-       
         .then(dbUsersData => {
             if(!dbUsersData) {
                 res.status(404).json({message: 'No User with this particular ID!'});
@@ -35,7 +34,6 @@ const usersController = {
             res.status(400).json(err)
         })
     },
-
     updateUsers({params, body}, res) {
         Users.findOneAndUpdate({_id: params.id}, body, {new: true, runValidators: true})
         .then(dbUsersData => {
