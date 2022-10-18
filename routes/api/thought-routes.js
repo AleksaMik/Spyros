255225e65c43e9a611c8b7a763c312de45839089
AleksaMik <aleksam.2509@gmail.com>
2022-10-18 12:49:58 -0500

    thought-route.js upd

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -14,4 +14,5 @@ router.route('/:id').get(getThoughtById).put(updateThought).delete(deleteThought
 router.route('/:userId').post(createThought);
 router.route('/:thoughtId/reactions').post(addReaction);
 router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
+
 module.exports = router;
